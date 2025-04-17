62a665cfeb2f4a3b1b3eaa3f95e84261a8d68c29
Abdul-Rafay2005 <mrabdulrafay7788@gmail.com>
2025-04-17 23:45:24 +0500

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -4,34 +4,38 @@
 #include <QMainWindow>
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QLabel>
+#include <QGraphicsDropShadowEffect>
+#include <QGraphicsOpacityEffect>
+#include <QPropertyAnimation>
 #include "chatwindow.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void openGeneralDoctorChat();
-    void openPsychiatricDoctorChat();
-
 private:
-    Ui::MainWindow *ui;
     QWidget *centralWidget;
     QVBoxLayout *layout;
     QPushButton *generalDoctorBtn;
     QPushButton *psychiatricDoctorBtn;
+    QLabel *titleLabel;
+    QGraphicsDropShadowEffect *shadowEffect;
 
     void setupUI();
     void applyStyle();
+    void applyButtonStyle(QPushButton *button, const QString &color);
+
+private slots:
+    void openGeneralDoctorChat();
+    void openPsychiatricDoctorChat();
 };
 
 #endif // MAINWINDOW_H
